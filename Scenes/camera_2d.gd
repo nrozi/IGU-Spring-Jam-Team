5ad4260bf97cdd69fe8ead8807bfa833e4337db1
nrozi <nathanielrozinka@gmail.com>
2026-04-14 00:14:43 -0500

    modifying camera behavior. Added "slide"

--- Scenes/camera_2d.gd
+++ Scenes/camera_2d.gd
@@ -1,16 +1,23 @@
 extends Camera2D
 
-@export var cam_speed: float = 1000
+@export var cam_speed: float = 1
+@export var cam_lag = 1
+var velocity: Vector2
 
 func _ready() -> void:
 	make_current()
 	
 func _physics_process(delta: float) -> void:
 	if Input.is_action_pressed("cam_left"):
-		position.x -= cam_speed * delta
+		velocity.x = -cam_speed * delta * 100000
 	if Input.is_action_pressed("cam_right"):
-		position.x += cam_speed * delta
+		velocity.x = cam_speed * delta * 100000
 	if Input.is_action_pressed("cam_up"):
-		position.y -= cam_speed * delta
+		velocity.y = -cam_speed * delta * 100000
 	if Input.is_action_pressed("cam_down"):
-		position.y += cam_speed * delta
+		velocity.y = cam_speed * delta * 100000
+	else:
+		velocity.x = move_toward(velocity.x, 0, cam_lag * delta * 10000)
+		velocity.y = move_toward(velocity.y, 0, cam_lag * delta * 10000)
+
+	position += velocity * delta
