3d9b96693cb3e47d809405eb9aed9a809adca16a
nrozi <nathanielrozinka@gmail.com>
2026-04-14 23:35:23 -0500

    added right edge pathfinding to graph. Still no behavior change

--- Scenes/static_platforms.gd
+++ Scenes/static_platforms.gd
@@ -40,6 +40,7 @@ func build_graph():
 func add_graph_points():
 	for tile in used_tiles:
 		add_left_edge_point(tile)
+		add_right_edge_point(tile)
 
 func add_left_edge_point(tile: Vector2i):
 	if tile_above_exist(tile):
@@ -58,6 +59,23 @@ func add_left_edge_point(tile: Vector2i):
 			point_info_list[point_info_list.find(existing_point_id)].is_left_edge = true
 			add_visual_point(tile_above)
 	
+func add_right_edge_point(tile: Vector2i):
+	if tile_above_exist(tile):
+		return
+	if get_cell_source_id(Vector2i(tile.x + 1, tile.y)) == -1:
+		var tile_above = Vector2i(tile.x, tile.y - 1)
+		var existing_point_id = tile_already_exist_in_graph(tile_above)
+		if(existing_point_id == -1):
+			var point_id = a_star_graph.get_available_point_id()
+			var point_info = point_info.new(point_id, Vector2i(map_to_local(tile_above)))
+			point_info.is_right_edge = true
+			point_info_list.append(point_info)
+			a_star_graph.add_point(point_id, Vector2i(map_to_local(tile_above)))
+			add_visual_point(tile_above)
+		else:
+			point_info_list[point_info_list.find(existing_point_id)].is_right_edge = true
+			add_visual_point(tile_above)
+	
 func tile_above_exist(tile: Vector2i) -> bool:
 	if get_cell_source_id(Vector2i(tile.x, tile.y - 1)) == cell_empty:
 		return false
@@ -66,8 +84,9 @@ func tile_above_exist(tile: Vector2i) -> bool:
 func add_visual_point(tile_above):
 	if !show_debug_graph:
 		return
-	graph_point_inst.position = map_to_local(tile_above)
-	add_child(graph_point_inst)
+	var graph_point = graph_point.instantiate()
+	graph_point.position = map_to_local(tile_above)
+	add_child(graph_point)
 
 func tile_already_exist_in_graph(tile: Vector2i):
 	var local_pos = map_to_local(tile)
